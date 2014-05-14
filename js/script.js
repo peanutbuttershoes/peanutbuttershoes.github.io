476aca4755fe611cd97cb36ad8e58bac79333209
Derek Sneed <peanutbuttershoes@gmail.com>
2014-05-13 22:20:42 -0500

    this one counts for real this time k

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+	var $container = $('#content');
+	// initialize Masonry after all images have loaded  
+	$container.imagesLoaded( function() {
+  	$container.masonry();
+	});
+
 	// Initialize Masonry
 	$('#content').masonry({
 		columnWidth: 300,
